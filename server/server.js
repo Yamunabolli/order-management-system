8259a1e1e620eba7891e2bd703348a156102b0c1
yamuna bolli <bolliyamuna8241@gmail.com>
2026-03-31 14:22:08 +0530

    change versions

--- server/server.js
+++ server/server.js
@@ -56,9 +56,8 @@ app.patch("/orders/:id", (req, res) => {
   res.json({ success: true });
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000 🚀");
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT)
 
 setInterval(() => {
   const newOrder = {
